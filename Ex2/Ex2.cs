522b4fbdf7d1da32101adbd01d75296ee64bc632
Viktoria <kirvikanik@gmail.com>
2022-07-16 15:14:07 +0300

    задача 2,4

---
+++ Ex2/Ex2.cs
@@ -0,0 +1,13 @@
+﻿//задача 2 на примере одной связки чисел, остальные выполняются также,
+//просто меняются числа
+
+int a = 5;
+int b = 7;
+
+
+int max = a;
+if(a > max) max = a;
+if(b > max) max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
